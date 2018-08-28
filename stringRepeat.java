f2f6bbb13037ecde1e69303195937e09ba7c1528
washingtonBrewologist <42781568+washingtonBrewologist@users.noreply.github.com>
2018-08-28 11:03:41 -0700

    Update stringRepeat.java

--- stringRepeat.java
+++ stringRepeat.java
@@ -1,9 +1,15 @@
+/*
+Write a recursive method repeat that accepts a string s and an integer n as 
+parameters and that returns a String consisting of n copies of s.
+*/
+
+
 public class repeatRecursion {
 
     public static void main(String[] args) {
 
 
-        System.out.println(repeat("", 500));
+        System.out.println(repeat());
 
     }
     public static String repeat(String s, int n) {
