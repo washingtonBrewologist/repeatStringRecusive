f2f2b10829ba171aadc764bab2d86df432625805
washingtonBrewologist <42781568+washingtonBrewologist@users.noreply.github.com>
2018-08-28 11:24:54 -0700

    Update stringRepeat.java

--- stringRepeat.java
+++ stringRepeat.java
@@ -3,21 +3,13 @@ Write a recursive method repeat that accepts a string s and an integer n as
 parameters and that returns a String consisting of n copies of s.
 */
 
-
-public class repeatRecursion {
-
-    public static void main(String[] args) {
-
-
-        System.out.println(repeat());
-
-    }
-    public static String repeat(String s, int n) {
+public static String repeat(String s, int n) {
         if (n < 0) {
             throw new IllegalArgumentException();
-
+        } else if (n == 0) {
+            return "";
         } else {
-            return new String(new char[n]).replace("\0", s);
+            return s + repeat(s, n -1);
         }
 
     }
